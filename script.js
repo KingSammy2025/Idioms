ed93386440a257d91bb3d5178d28045bfe9b8c5c
KingSammy2025 <o.ibikunle-philips@share.epsb.ca>
2024-10-21 14:19:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("idiom-btn").addEventListener("click", idiom1);
+function idiom1() {
+  let output = "corners";
+  alert(output);
+}
+
+document.getElementById("idiom-btn2").addEventListener("click", idiom2);
+function idiom2() {
+  let output = "bush";
+  alert(output);
+}
+
+document.getElementById("idiom-btn3").addEventListener("click", idiom3);
+function idiom3() {
+  let output = "sack";
+  alert(output);
+}
+
+document.getElementById("idiom-btn4").addEventListener("click", idiom4);
+function idiom4() {
+  let output = "ball";
+  alert(output);
+}
+
+document.getElementById("idiom-btn5").addEventListener("click", idiom5);
+function idiom5() {
+  let output = "good";
+  alert(output);
+}
